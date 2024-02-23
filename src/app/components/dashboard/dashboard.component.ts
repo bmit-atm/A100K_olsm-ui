d648c507ae121f06e293883b63fb6f12714b0867
bmit-atm <martin.atanasov@espas.ch>
2024-02-23 13:57:50 +0100

    added the html form

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.sass'
 })
 export class DashboardComponent {
+  fileName = '';
+  constructor() {}
 
+
+  
 }
