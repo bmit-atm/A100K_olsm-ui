9dbdd957d5aaf8f292292afb9220aead017dc0d2
bmit-atm <martin.atanasov@espas.ch>
2024-02-23 13:11:50 +0100

    added enviroments file

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api', // Local Server
+    //apiUrl: 'http://localhost:3000/api', // Live Server
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api', // Local Server
+    //apiUrl: 'http://localhost:3000/api', // Live Server
+};
